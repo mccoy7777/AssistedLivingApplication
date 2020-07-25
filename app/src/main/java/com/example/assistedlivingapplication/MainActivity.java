2fa632ba1bc7df0efb5ece1af02c6b1cc4a36e27
mccoy7777 <58072696+mccoy7777@users.noreply.github.com>
2020-07-25 18:45:44 +0100

    Second change

--- app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
+++ app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //My new application
+        //My new application name
 
     }
 }
