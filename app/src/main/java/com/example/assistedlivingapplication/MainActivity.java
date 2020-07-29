81ee8ac1f8d32517de4f44233e2167263951d533
mccoy7777 <58072696+mccoy7777@users.noreply.github.com>
2020-07-29 18:52:43 +0100

    Can fetch data now from FireBase database

--- app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
+++ app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
@@ -16,14 +16,51 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declare variables
+    TextView name, email, phone, dob;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign variables
+        name = findViewById(R.id.tv_name);
+        email = findViewById(R.id.tv_email);
+        phone = findViewById(R.id.tv_phone);
+        dob = findViewById(R.id.tv_age);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                name.setText(documentSnapshot.getString("Name"));
+                phone.setText(documentSnapshot.getString("Phone"));
+                email.setText(documentSnapshot.getString("Email"));
+                dob.setText(documentSnapshot.getString("Dob"));
+            }//end of onEvent method
+
+        });//end of EventListener
 
 
     }//End of OnCreate Method
