a3beaaca22d87c839f223460cd58d7145ee04dc9
mccoy7777 <58072696+mccoy7777@users.noreply.github.com>
2020-07-29 19:46:31 +0100

    Added Reset Password By Email Link

--- app/src/main/java/com/example/assistedlivingapplication/Login.java
+++ app/src/main/java/com/example/assistedlivingapplication/Login.java
@@ -1,8 +1,10 @@
 package com.example.assistedlivingapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,7 +27,7 @@ public class Login extends AppCompatActivity {
     //Declare Variables
     EditText etEmail, etPassword;
     Button btnLogin;
-    TextView tvLogin;
+    TextView tvLogin, tvForgotPassword;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
 
@@ -39,6 +43,7 @@ public class Login extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         btnLogin = findViewById(R.id.btn_login);
         tvLogin = findViewById(R.id.tv_login);
+        tvForgotPassword = findViewById(R.id.tv_forgotpassword);
 
         //Create new onClickListener for Login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -95,6 +100,55 @@ public class Login extends AppCompatActivity {
             }//end of OnClick method
 
         });//End of onClickListener method
+
+        //Create onClickListener for Forgot Password link
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Create new edit text view and use alert dialogue class to prompt user to reset password
+                final EditText resetEmail = new EditText(view.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+                passwordResetDialog.setTitle("Reset Password");
+                passwordResetDialog.setMessage("Enter your email to receive reset link");
+                passwordResetDialog.setView(resetEmail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //extract the email and send the reset link
+                        String email = resetEmail.getText().toString();
+                        fAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this, "Reset Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+                            }//end of onSuccess method
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error, Reset Link not sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }//end of onFailure method
+                        });//end of onFailure Listener
+
+                    }//end of onClick method
+
+                });//end of onClickListener
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Close the dialogue
+
+                    }//end of onClick method
+
+                });//end of onClickListener
+
+                passwordResetDialog.create().show();
+
+            }//end of onClick method
+
+        });//end of onClickListener
 
 
     }//End of OnCreate method
