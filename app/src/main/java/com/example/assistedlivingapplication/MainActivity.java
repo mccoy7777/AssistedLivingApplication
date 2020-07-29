c8dde82d731665b66f0da8f6d71e3120c6780463
mccoy7777 <58072696+mccoy7777@users.noreply.github.com>
2020-07-29 23:19:16 +0100

    Added Button So User Can Change Password Via App

--- app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
+++ app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.assistedlivingapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,9 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     //Declare variables
     TextView name, email, phone, dob;
+    Button changePassword;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+
+    FirebaseUser user;
 
 
     @Override
@@ -44,10 +51,14 @@ public class MainActivity extends AppCompatActivity {
         phone = findViewById(R.id.tv_phone);
         dob = findViewById(R.id.tv_age);
 
+        changePassword = findViewById(R.id.btn_changepassword);
+
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
 
         userId = fAuth.getCurrentUser().getUid();
+
+        user = fAuth.getCurrentUser();
 
         DocumentReference documentReference = fStore.collection("users").document(userId);
 
@@ -61,6 +72,56 @@ public class MainActivity extends AppCompatActivity {
             }//end of onEvent method
 
         });//end of EventListener
+
+        //Setup onClickListener to change the password when button is clicked
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Create new edit text view and use alert dialogue class to prompt user to reset password
+                final EditText changePassword = new EditText(view.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+                passwordResetDialog.setTitle("Change Password");
+                passwordResetDialog.setMessage("Enter New Password (At Least 6 Characters Long)");
+                passwordResetDialog.setView(changePassword);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //create variable for new password and use firebase to update it
+                        String newPassword = changePassword.getText().toString();
+                        user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Password has been changed successfully", Toast.LENGTH_SHORT).show();
+                            }//end of onSuccess method
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Password Change Failed", Toast.LENGTH_SHORT).show();
+                            }//end of onFailure method
+
+                        });//end of onSuccess Listener method
+
+                    }//end of onClick method
+
+                });//end of onClickListener
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Close the dialogue
+
+                    }//end of onClick method
+
+                });//end of onClickListener
+
+                passwordResetDialog.create().show();
+
+            }//end of onClick method
+
+        });//end of onClickListener method
 
 
     }//End of OnCreate Method
