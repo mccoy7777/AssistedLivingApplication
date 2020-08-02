56b69fa4da81d4e3826a4ee7548c7ecf57533594
mccoy7777 <58072696+mccoy7777@users.noreply.github.com>
2020-08-02 20:21:26 +0100

    Changed the layout for the main screen, fixed layout and created new classes for prescription service and daily tasks

--- app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
+++ app/src/main/java/com/example/assistedlivingapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.assistedlivingapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,6 +13,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,18 +30,56 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Nullable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Declare variables
+    private CardView myProfile, dailyTasks, prescriptionService, panicButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Assign variables
+        myProfile = (CardView) findViewById(R.id.my_profile);
+        dailyTasks = (CardView) findViewById(R.id.daily_tasks);
+        prescriptionService = (CardView) findViewById(R.id.prescription_service);
+        panicButton = (CardView) findViewById(R.id.panic_button);
+
+        //Add Click Listener to each CardView
+        myProfile.setOnClickListener(this);
+        dailyTasks.setOnClickListener(this);
+        prescriptionService.setOnClickListener(this);
+        panicButton.setOnClickListener(this);
+
 
     }//End of OnCreate Method
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.my_profile : i = new Intent(this, UserProfile.class);
+            startActivity(i);
+            break;
+            case R.id.daily_tasks : i = new Intent (this, DailyTasks.class);
+            startActivity(i);
+            break;
+            case R.id.prescription_service : i = new Intent(this, PrescriptionService.class);
+            startActivity(i);
+            break;
+            case R.id.panic_button : i = new Intent (this, PanicButton.class);
+            startActivity(i);
+            break;
+            default: break;
+        }
+    }
 
     //Create method that allows user to logout of application
     public void logout (View view){
